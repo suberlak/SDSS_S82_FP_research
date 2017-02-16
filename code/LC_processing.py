679c17646e558bcca97c89b3d5a09e2b72081811
suberlak <suberlak@uw.edu>
2017-02-16 15:21:35 -0800

    cleaned up the code, allowing console arguments for LC_processing.py , still hard-coded in processPatch.py

--- code/LC_processing.py
+++ code/LC_processing.py
@@ -39,16 +39,75 @@ import os
 import numpy as np
 
 
-# make python aware of my packages...
 import sys
 
-###
-### setting these two is CRUCIAL 
-###
-execution_environment = 'mac'  # or 'typhoon'
-site = 'NCSA'
+# use either console arguments to make this things easier, 
+# or allow to set things in stone in the code ... 
+
+# in the future: 
+# make more user-friendly https://docs.python.org/3.3/library/argparse.html 
+
+arg1_m = ['m', 'mac', 'macbook']
+arg1_t = ['t', 'typhoon', 'workstation']
+arg2_n = ['1', 'NCSA']
+arg2_i = ['2', 'IN2P3']
+
+execution_environment = None
+site = None
+if len(sys.argv) == 2 : 
+    print('Insufficient arguments : need  to call with   arg1   arg2  *arg3')
+    print('arg1 : which computer we are on' )
+    print(arg1_m); print(arg1_t)
+    print('arg2: which site should we process patches from ')
+    print(arg2_n); print(arg2_i)
+    print('arg3 : limit number of rows ? (optional)')
+   
+    print('\nFor example:')
+    print('python LC_processing.py  m 1')
+    
+    sys.exit()
+
+if len(sys.argv) > 2 :
+    ex = sys.argv[1]
+    if ex in  arg1_m:
+        execution_environment = 'mac'
+    elif ex in arg1_t:
+        execution_environment = 'typhoon'
+    else : 
+        print('Invalid argument 1 - need to choose from ')
+        print(arg1_m); print(arg1_t)
+        sys.exit()
+
+    s = sys.argv[2]
+    if s in arg2_n:
+        site = 'NCSA'
+    elif s in arg2_i:
+        site = 'IN2P3'
+    else  : 
+        print('Invalid argument 2 - need to choose from ')
+        print(arg2_n); print(arg2_i)
+        sys.exit()
+    
+    if site is not None and execution_environment is not None : 
+        print('Using execution_environment = %s and site=%s'%(execution_environment, site))
+
+    if len(sys.argv) > 3 :
+         limitNrows = int(sys.argv[3])
+         print('Limiting rows used from each patch file to  %d'%limitNrows)
+    
+if len(sys.argv) == 1 : 
+    # Need to set things manually in the code , since no arguments are provided
+    print('No arguments provided from the console ... ')
+    ###
+    ### setting these two is CRUCIAL 
+    ###
+    execution_environment = 'mac'
+    site = 'NCSA'
+    print('Using execution_environment = %s and site=%s'%(execution_environment, site))
 
 
+
+# Need this to locate my packages ...
 if execution_environment == 'mac' : 
     path_to_home = '/Users/chris/GradResearch/'
 
@@ -56,6 +115,7 @@ elif execution_environment == 'typhoon' :
     path_to_home = '/astro/users/suberlak/' 
 
 sys.path.insert(0, path_to_home + 'SDSS_S82_FP_research/packages/')
+
 
 import processPatch as procP
 
@@ -72,39 +132,21 @@ elif  execution_environment == 'typhoon' :
 
 
 # Define patches which we will process in this run ... 
-lToDoFilt= []
+filter_patch_files = []
 if site == 'NCSA':
-    patches = ['00_21','22_43','44_65', '66_87' ,'88_109','110_131', '132_153', 
-               '154_175',  '176_181', '365_387', '388_409']  # NCSA 
+	patches = ['00_21']  # temporary, for just one patch...
+    #patches = ['00_21','22_43','44_65', '66_87' ,'88_109','110_131', '132_153', 
+    #           '154_175',  '176_181', '365_387', '388_409']  # NCSA 
 if site == 'IN2P3':
 	patches = ['155_176', '176_197','197_218', '218_239', '239_260', '260_281', '281_302', 
                '302_323','323_344', '344_365', '365_386']  # IN2P3
-# 
+
 for patch in patches  :
     for filter in 'ugriz':
-        lToDoFilt.append(filter + patch + '.csv')
+        filter_patch_files.append(filter + patch + '.csv')
     
 # Run this for processing : calculation of over 27 metrics per lightcurve per band 
-for name in lToDoFilt[16:]:
-    procP.process_patch_minimal(name, DirIn, DirOut)
-
-
-
-
-
-# 
-# obsolete : if no use for that, remove entirely... 
-# 
-#lProc = []
-#lProc += [each for each in os.listdir(DirOut) if each.endswith('.csv')]
-
-#lProc = [name[5:-5] for name in lProc]
-#lToDo = os.listdir(DirIn)
-#lToDoComp = [name[:-3] for name in lToDo]
-#if len(lProc) > 0 : 
-#    maskDone = np.in1d(lToDoComp,lProc)
-#    lToDoFilt =  np.array(lToDoComp)[~maskDone]
-#else:
-#    lToDoFilt = lToDoComp
+for name in filter_patch_files :
+    procP.process_patch(name, DirIn, DirOut,limitNrows=limitNrows)
 
 
--- packages/processPatch.py
+++ packages/processPatch.py
@@ -3,7 +3,19 @@ import pandas as pd
 import numpy as np
 import os 
 import sys
-sys.path.insert(0, '/astro/users/suberlak/S13Agg_analysis/packages/')
+
+execution_environment = 'typhoon'  # or 'mac'
+site = 'NCSA'
+
+
+if execution_environment == 'mac' : 
+    path_to_home = '/Users/chris/GradResearch/'
+
+elif execution_environment == 'typhoon' :
+    path_to_home = '/astro/users/suberlak/' 
+
+sys.path.insert(0, path_to_home + 'SDSS_S82_FP_research/packages/')
+
 pd.options.mode.chained_assignment = None
 
 # God, Relieve me of the bondage of self,
@@ -62,9 +74,7 @@ def process_patch(name, DirIn, DirOut, limitNrows=None, calc_seasonal_metrics=No
     '''
 
     print('Processing filter_patch file %s' % name)
-    #DirIn = '/astro/store/pogo4/s13_stripe82/forced_phot_lt_23/NCSA/'
-    #DirOut = '/astro/store/scratch/tmp/suberlak/s13_stripe82/forced_phot_lt_23/NCSA/'
-
+    
     # read in the raw lightcurve... 
     if limitNrows is not None:
         fp_data = pd.read_csv(DirIn+name+'.gz', compression='gzip',  
@@ -75,7 +85,8 @@ def process_patch(name, DirIn, DirOut, limitNrows=None, calc_seasonal_metrics=No
     #   
     ##########  STEP 1 : single-epoch data ###########  
     #
-    ### at the very beginnig : convert  Flux from erg/cm2/sec/Hz  to Jansky
+    ### at the very beginnig : 
+    # convert  Flux from erg/cm2/sec/Hz  to Jansky
     # 1 Jy = 1.0E-26 W/m^2/Hz = 1.0E-23 erg/s/cm^2/Hz
 
     fp_data['psfFlux'] = fp_data['psfFlux']  * 1E23 
@@ -115,25 +126,17 @@ def process_patch(name, DirIn, DirOut, limitNrows=None, calc_seasonal_metrics=No
     fp_data['faintMedian'] = np.nan
     fp_data['faintTwoSigma'] = np.nan
     fp_data['faintRMS'] = np.nan
+
     # calculate the faint replacement only for faint points...
     print('Faint points treatment...')
     fp_data.ix[mask, 'faintMean'] = faintF.calculate_mean(fp_data['psfFlux'][mask].values,fp_data['psfFluxErr'][mask].values)
     fp_data.ix[mask, 'faintMedian'] = faintF.calculate_median(fp_data['psfFlux'][mask].values,fp_data['psfFluxErr'][mask].values)
     fp_data.ix[mask, 'faintTwoSigma'] = faintF.calculate_2sigma(fp_data['psfFlux'][mask].values,fp_data['psfFluxErr'][mask].values)
     fp_data.ix[mask, 'faintRMS'] = faintF.calculate_rms(fp_data['psfFlux'][mask].values,fp_data['psfFluxErr'][mask].values)
-    ## If one wants, at this stage we may replace all fluxes by Mean or Median
-    # I would keep both, and then use either, as appropriate.
-    # This replaces all rows where flag == 1
-    #rows = fp_data['flag'] == 1
-    #fp_data.ix[rows, 'psfFlux'] = fp_data.ix[rows, 'faintMean']
+   
     # 
     ######################### SAVING OUTPUT (unneccesarily takes time...)
-    # as a new csv....
-    #path = DirOut+'Proc_'+name
-    #print('Saving processed file to %s'% path)
-    #fp_data.to_csv(path)  
-
-    # Save the diagnostics..
+    #
     print('Saving the diagnostics...')
     path = DirOut+'Proc_'+name
     diagFile = path+'.diag'
@@ -218,7 +221,7 @@ def process_patch(name, DirIn, DirOut, limitNrows=None, calc_seasonal_metrics=No
     ##########  STEP 3 : Variable Candidates ###########  
     # 
     if calc_seasonal_metrics is None : 
-        return varMetricsFull
+        return 
     print('Identifying Variable Candidates...')
     #
     # for variable candidates we calculate  metrics for 
@@ -228,22 +231,12 @@ def process_patch(name, DirIn, DirOut, limitNrows=None, calc_seasonal_metrics=No
     # 
 
     # Check how many sources  are variable... 
-    # i.e. have either  sigma > 0 or chi2 > 1
-    #m1 = (varMetricsFull['sigmaFull'] > 0)  # 234883
-    #m2 =  (varMetricsFull['chi2DOF'] > 1).values   # 346886
-    #m3 = (varMetricsFull['chi2R'] > 1).values      # 172602
-    #m= np.ma.mask_or(m3, np.ma.mask_or(m1,m2))
-    #print('Out of %d objects,  %d fulfill  sigma>0 or chi2R>1 or chi2DOF>1' % (len(m), np.sum(m)))
-
     # The  '3 sigma' definition (more strict) ...
     m1 = (varMetricsFull['sigmaFull'] > 0)
     N = varMetricsFull['N'] #  number of pts per lightcurve 
     m4 = varMetricsFull['chi2DOF'] > (1 + 3.0 * np.sqrt(2 / N )) ##  170517
     m5 = varMetricsFull['chi2R'] > (1 + 3.0 * np.sqrt(2 / N))   ## 37464
-    #np.sum(m4|m5)  # one or the other   170534#
-    #np.sum(m1|m4|m5) # 265981   sigma OR  chi2R  OR chi2DOF 
-    #np.sum(m1*(m4|m5)) # 139436     sigma AND  (chi2R OR chi2DOF)
-
+ 
     # save the diagnostics about variability to a file...
     diagFile = path+'.diag'
     file = open(diagFile, "w")
@@ -274,7 +267,7 @@ def process_patch(name, DirIn, DirOut, limitNrows=None, calc_seasonal_metrics=No
     print('Saved variable candidates info to %s' %diagFile)
 
     if calc_seasonal_metrics is None : 
-        return varMetricsFull
+        return 
     #
     ################ SEASONAL METRICS #################
     #
@@ -331,113 +324,6 @@ def process_patch(name, DirIn, DirOut, limitNrows=None, calc_seasonal_metrics=No
     path = DirOut + 'FullSeasVar'+name
     varMetricsFullSeasonal.to_csv(path)
     print('Saving Full Seasonally binned LC statistics to %s'%path)
-
-
-
-
-# not needed to use, since we don't want to calculate any 
-# metrics  based on magnitudes... 
-
-def process_patch_minimal(name, DirIn, DirOut, limitNrows=None):
-    ''' A variation of process_patch : ONLY calculate sigma, mu , 
-    based on psfMag and psfMagErr,  i.e.  psfFlux, psfFluxErr 
-    after faint point treatment, translated to magnitudes  
-    '''
-    print('Processing filter_patch file %s' % name)
-    #DirIn = '/astro/store/pogo4/s13_stripe82/forced_phot_lt_23/NCSA/'
-    #DirOut = '/astro/store/scratch/tmp/suberlak/s13_stripe82/forced_phot_lt_23/NCSA/'
-
-    # read in the raw lightcurve... 
-    if limitNrows is not None:
-        fp_data = pd.read_csv(DirIn+name+'.gz', compression='gzip',  
-                     usecols=['objectId', 'mjd', 'psfFlux', 'psfFluxErr'], nrows=limitNrows)
-    else : 
-        fp_data = pd.read_csv(DirIn+name+'.gz', compression='gzip',  
-                     usecols=['objectId', 'mjd', 'psfFlux', 'psfFluxErr'])
-    #
-    ##########  STEP 1 : single-epoch data ###########  
-    #
-
-    ####  first drop all NaNs  in psfFlux...      
-    m1  = np.isnan(fp_data['psfFlux'])  # True if NaN  
-    m2 =  ~np.isfinite(fp_data['psfFlux']) #  True if not finite  
-    m  = m1 | m2  # a logical or 
-    if np.sum(m) > 0 :  # only apply if there is anything to drop ... 
-        fp_data.drop(m.index[m], inplace=True)  # drop entire rows 
-        print('Okay, we dropped %d rows where psfFlux is NaN or inf'%np.sum(m))
-
-    #### check to make sure that there are no NaN or 0 psfFluxErr... 
-    m1  = np.isnan(fp_data['psfFluxErr'])  # True if NaN  
-    m2 =  ~np.isfinite(fp_data['psfFluxErr']) #  True if not finite
-    m3 =   fp_data['psfFluxErr'].values == 0 # True if Err == 0  (IN2P3 problem...)
-    m  = m1 | m2 | m3  # a logical or 
-    if np.sum(m) > 0 :  # only apply if there is anything to drop ... 
-        fp_data.drop(m.index[m], inplace=True)
-        print('Okay, we dropped %d rows where psfFluxErr is NaN or inf'%np.sum(m))
-
-    # make a new column, fill with 0's
-    fp_data['flagFaint'] = 0
-
-    # mask those rows that correspond to SNR < 2
-    mask = (fp_data['psfFlux'].values / fp_data['psfFluxErr'].values) < 2
-
-    # print info how many points are affected
-    print('There are %d points of %d that have SNR<2' %(np.sum(mask),len(mask)))
-
-    # set flag at those rows to 1
-    fp_data.ix[mask, 'flagFaint'] = 1
-
-    # make new columns for  Mean  Median  2 sigma...
-    fp_data['faintMean'] = np.nan
-    fp_data['faintMedian'] = np.nan
-    fp_data['faintTwoSigma'] = np.nan
-    fp_data['faintRMS'] = np.nan
-    # calculate the faint replacement only for faint points...
-    print('Faint points treatment...')
-    fp_data.ix[mask, 'faintMean'] = faintF.calculate_mean(fp_data['psfFlux'][mask].values,fp_data['psfFluxErr'][mask].values)
-    fp_data.ix[mask, 'faintMedian'] = faintF.calculate_median(fp_data['psfFlux'][mask].values,fp_data['psfFluxErr'][mask].values)
-    fp_data.ix[mask, 'faintTwoSigma'] = faintF.calculate_2sigma(fp_data['psfFlux'][mask].values,fp_data['psfFluxErr'][mask].values)
-    fp_data.ix[mask, 'faintRMS'] = faintF.calculate_rms(fp_data['psfFlux'][mask].values,fp_data['psfFluxErr'][mask].values)
-    
-    ##########  STEP 2 : Derived Quantities ###########  
-    #
-
-    ####  replace all psfFlux  where SNR < 2  with  faintMean  
-    rows = fp_data['flagFaint'] == 1
-    fp_data.ix[rows, 'psfFlux'] = fp_data.ix[rows, 'faintMean']
-    
-    def flux2absigma(flux, fluxsigma):
-      """Compute AB mag sigma given flux and flux sigma"""
-      FIVE_OVER_2LOG10 = 1.085736204758129569
-      return FIVE_OVER_2LOG10 * fluxsigma / flux;
-
-
-    def flux2ab(flux):
-      """Compute AB mag given flux"""
-      return -2.5 * np.log10(flux) - 48.6;
-    
-    # Calculate psfMag , psfMagErr from psfFlux,  psfFluxErr
-
-    fp_data['psfMag'] = flux2ab(fp_data['psfFlux'])
-    fp_data['psfMagErr'] = flux2absigma(fp_data['psfFlux'],fp_data['psfFluxErr'])
-
-
-    # group by objectId to calculate full LC variability characteristics 
-    print('Calculating the full LC statistics...')
-    grouped = fp_data.groupby('objectId')
-
-    # Calculate only sigmaFullMag  muFullMag   for full lightcurves from 
-    # magnitudes     
-    varMetricsFull = grouped.apply(varF.computeVarMetricsMagOnly)
-    print('Calculating metrics for full lightcurves is finished')
-
-   
-    path = DirOut +'Var/'+ 'VarSigMag'+name
-    print('Saving varMetricsFull...')
-    varMetricsFull.to_csv(path)
-    print('Saved Full, unbinned  LC statistics to %s'%path)
-
-
 
 
 
