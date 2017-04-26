03a8b8727c2f65dda70c727df70bcfc7418c656c
suberlak <suberlak@uw.edu>
2017-04-25 18:34:40 -0700

    small edit

--- code/Sample_LC_extract.py
+++ code/Sample_LC_extract.py
@@ -29,84 +29,82 @@ patches_to_read = np.unique(sample_data['patch']).data
 # for our variable objects ... 
 
 
-#for patch in patches_to_read : 
-patch = patches_to_read[0]   # for testing , only 00_21, otherwise we'd run over all ... 
+for patch in patches_to_read : 
+#patch = patches_to_read[0]   # for testing , only 00_21, otherwise we'd run over all ... 
+	# choose objects that we want to extract from that patch ... 
+	mask_rows= sample_data['patch'].data == patch
+	objects_in_patch = sample_data['objectId'].data[mask_rows]
+	print('There are %d objects of interest in patch %s ' % (len(objects_in_patch), patch))
+
+	# for each patch, there is a different site.  On the mac I only have 00_21 which is from NCSA,
+	# but in general there are two sites - for each patch it's either NCSA or IN2P3 
+	site = sample_data['site'].data[mask_rows][0]
 
 
-# choose objects that we want to extract from that patch ... 
-mask_rows= sample_data['patch'].data == patch
-objects_in_patch = sample_data['objectId'].data[mask_rows]
-print('There are %d objects of interest in patch %s ' % (len(objects_in_patch), patch))
+	# Initialize the dictionary to store all photometry to keep
+	# from a given filter-patch file : 
+	subpatch = {}
 
-# for each patch, there is a different site.  On the mac I only have 00_21 which is from NCSA,
-# but in general there are two sites - for each patch it's either NCSA or IN2P3 
-site = sample_data['site'].data[mask_rows][0]
+	# read all relevant rows from all filters in a given patch... 
+	for filter in 'ugriz' : 
+	    fname = filter+patch+'.csv.gz'
+	    print('\n Reading in %s'%fname)
+	    DirIn = '/astro/store/pogo4/s13_stripe82/forced_phot_lt_23/'+site+'/'
+	    filter_patch = Table.read(DirIn+fname, format='csv')
+	    
+	    # How many rows we will extract in this filter ... 
+	    n_raw = len(filter_patch)
+	    print('In this filter patch there are %d raw photometric measurements '%n_raw)
 
+	    mask_grab = np.in1d(filter_patch['objectId'].data, objects_in_patch)
+	    n_raw_grab = np.sum(mask_grab)
+	    print('%d of these pertain to objects of  interest, and we extract them '%n_raw_grab)
 
-# Initialize the dictionary to store all photometry to keep
-# from a given filter-patch file : 
-subpatch = {}
+	    # select only specific columns from each patch-filter file, to speed up execution 
+	    columns_of_interest = ['objectId', 'mjd', 'psfFlux', 'psfFluxErr']
+	    subpatch[filter] = filter_patch[mask_grab][columns_of_interest]
 
-# read all relevant rows from all filters in a given patch... 
-for filter in 'ugriz' : 
-    fname = filter+patch+'.csv.gz'
-    print('\n Reading in %s'%fname)
-    DirIn = '/astro/store/pogo4/s13_stripe82/forced_phot_lt_23/'+site+'/'
-    filter_patch = Table.read(DirIn+fname, format='csv')
-    
-    # How many rows we will extract in this filter ... 
-    n_raw = len(filter_patch)
-    print('In this filter patch there are %d raw photometric measurements '%n_raw)
+	    new_col = Column(name='filter',data= np.zeros_like(subpatch[filter]['objectId'], dtype='str'))
+	    new_col[:] = filter
+	    subpatch[filter].add_column(new_col)
 
-    mask_grab = np.in1d(filter_patch['objectId'].data, objects_in_patch)
-    n_raw_grab = np.sum(mask_grab)
-    print('%d of these pertain to objects of  interest, and we extract them '%n_raw_grab)
+	# Save as one-file-per-object ...
 
-    # select only specific columns from each patch-filter file, to speed up execution 
-    columns_of_interest = ['objectId', 'mjd', 'psfFlux', 'psfFluxErr']
-    subpatch[filter] = filter_patch[mask_grab][columns_of_interest]
+	# Iterate over all objectId's ,
+	# and pull photometry from  all filters
+	# into one lightcurve,
+	# column 'filter'  defines which 
+	# filter is the measurement from 
+	# data types here match precisely those in subpatch[filter],  inherited from individual patch-files ... 
+	LC_stack = Table(names = ['objectId', 'mjd', 'psfFlux', 'psfFluxErr', 'filter'], 
+	                            dtype=('i8', 'f8', 'f8','f8','S1'))
 
-    new_col = Column(name='filter',data= np.zeros_like(subpatch[filter]['objectId'], dtype='str'))
-    new_col[:] = filter
-    subpatch[filter].add_column(new_col)
+	cols_save = ['mjd', 'psfFlux', 'psfFluxErr', 'filter']
 
-# Save as one-file-per-object ...
+	for objectId in  objects_in_patch : 
+	    
+	    # start the stack : empty Table 
+	    object_LC_stack = Table(names = ['objectId', 'mjd', 'psfFlux', 'psfFluxErr', 'filter'], 
+	                            dtype=('i8', 'f8', 'f8','f8','S1'))
 
-# Iterate over all objectId's ,
-# and pull photometry from  all filters
-# into one lightcurve,
-# column 'filter'  defines which 
-# filter is the measurement from 
-# data types here match precisely those in subpatch[filter],  inherited from individual patch-files ... 
-LC_stack = Table(names = ['objectId', 'mjd', 'psfFlux', 'psfFluxErr', 'filter'], 
-                            dtype=('i8', 'f8', 'f8','f8','S1'))
+	    # append epochs for each filters row-by-row
+	    # using very efficient vstack : columns are 
+	    # exactly the same 
 
-cols_save = ['mjd', 'psfFlux', 'psfFluxErr', 'filter']
+	    for f in 'ugriz' :
+	        m =  subpatch[f]['objectId'] == objectId
+	        object_LC_stack = vstack([object_LC_stack, subpatch[f][m]])
+	    
+	    # Save stacked LC for a given object ...
+	    DirOut = '/astro/store/scratch/tmp/suberlak/s13_S82_2017/sample_LC/'
+	    lc_name = str(objectId) + '.csv'
+	    ascii.write(object_LC_stack[cols_save], DirOut + lc_name,  format='csv', overwrite=True)
 
-for objectId in  objects_in_patch : 
-    
-    # start the stack : empty Table 
-    object_LC_stack = Table(names = ['objectId', 'mjd', 'psfFlux', 'psfFluxErr', 'filter'], 
-                            dtype=('i8', 'f8', 'f8','f8','S1'))
+	    # Add LC for this object to the combined stack 
+	    LC_stack = vstack([LC_stack, object_LC_stack])
+	    
+	# Now, save the combined lightcurves too - in case it was easier to use such file...
 
-    # append epochs for each filters row-by-row
-    # using very efficient vstack : columns are 
-    # exactly the same 
-
-    for f in 'ugriz' :
-        m =  subpatch[f]['objectId'] == objectId
-        object_LC_stack = vstack([object_LC_stack, subpatch[f][m]])
-    
-    # Save stacked LC for a given object ...
-    DirOut = '../raw_data/sample_LC/'
-    lc_name = str(objectId) + '.csv'
-    ascii.write(object_LC_stack[cols_save], DirOut + lc_name,  format='csv', overwrite=True)
-
-    # Add LC for this object to the combined stack 
-    LC_stack = vstack([LC_stack, object_LC_stack])
-    
-# Now, save the combined lightcurves too - in case it was easier to use such file...
-
-DirOut = '../raw_data/sample_LC_combined/'
-ascii.write(LC_stack,DirOut + 'Sample_LC_'+str(len(len(objects_in_patch)))+'-objects.csv', 
-            format=csv,  overwrite=True )
+	DirOut = '/astro/store/scratch/tmp/suberlak/s13_S82_2017/sample_LC_combined/'
+	ascii.write(LC_stack,DirOut + 'Sample_LC_'+str(len(len(objects_in_patch)))+'-objects.csv', 
+	            format=csv,  overwrite=True )
