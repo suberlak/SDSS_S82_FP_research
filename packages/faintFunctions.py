052b77fe0cb9c3a90e37e1d54bb256e2903b0921
suberlak <suberlak@uw.edu>
2017-05-10 18:41:16 -0700

    updated rms and mean problems

--- packages/faintFunctions.py
+++ packages/faintFunctions.py
@@ -8,25 +8,37 @@ import numpy as np
 
 def calculate_mean(psfFlux, psfFluxErr):
     xObs = psfFlux / psfFluxErr
-    # Calculate my results
-    xMean = (1/ (norm.sf(-xObs )*np.sqrt(2*np.pi))) * np.exp(-(xObs**2.0) / 2.0) + xObs
+    
+    # Initialize as zeros.  if xObs is so small ( eg < -50 ) that 
+    # we're getting norm.sf(-xObs) = 0, then we can't divide by that 
+    # number --> only divide if sf is nonzero
+    xMean = np.zeros_like(xObs)  
+    
+    # calculate the survival function 
+    sf = norm.sf(-xObs )
+    ind = np.where(sf !=0 )
+
+    # only divide by sf where the divisor is nonzero ... 
+    xMean[ind] = (1/ (sf[ind]*np.sqrt(2*np.pi))) * np.exp(-(xObs[ind]**2.0) / 2.0) + xObs[ind]
     F_mean = xMean * psfFluxErr
     return F_mean
 
 def calculate_median(psfFlux, psfFluxErr):
-    x0= -psfFlux / psfFluxErr
-    return psfFluxErr*norm.ppf((1+norm.cdf(x0))/2.0) + psfFlux
+    xObs = psfFlux / psfFluxErr
+    return psfFluxErr*norm.ppf((1+norm.cdf(-xObs))/2.0) + psfFlux
 
 
 def calculate_2sigma(psfFlux, psfFluxErr):
-    x0= -psfFlux / psfFluxErr
-    return psfFlux + psfFluxErr * norm.isf(0.05 * norm.sf(x0))
+    xObs = psfFlux / psfFluxErr
+    return psfFlux + psfFluxErr * norm.isf(0.05 * norm.sf(-xObs))
 
 def calculate_rms(psfFlux, psfFluxErr):
-    xObs = psfFlux / psfFluxErr
-    xMean = (1/ (norm.sf(-xObs )*np.sqrt(2*np.pi))) * np.exp(-(xObs**2.0) / 2.0) + xObs
-    delX = xObs - xMean
-    I1 = norm.sf(-xObs)
+    xObs  = psfFlux / psfFluxErr
+    xMean = calculate_mean(psfFlux, psfFluxErr) / psfFluxErr
+    delX  = xObs - xMean
+    I1    = norm.sf(-xObs)
     I0bysig2 = 0.5*erf(xObs/np.sqrt(2)) + (1.0/np.sqrt(2*np.pi))*np.exp(-(xObs**2.0) / 2.0)*(2*delX - xObs) + 0.5 + delX*delX*norm.sf(-xObs)
-    xRMS = np.sqrt(I0bysig2 / I1)
+
+    xRMS = np.ones_like(xObs)
+    xRMS[I0bysig2 > 0] = np.sqrt(I0bysig2[I0bysig2 > 0] / I1[I0bysig2 > 0])
     return  xRMS * psfFluxErr
